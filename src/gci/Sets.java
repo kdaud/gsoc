5454d9349b04cc548a032664a04589aa4470dd5f
kdaud <dkakumirizii@gmail.com>
2020-02-17 03:51:52 +0300

    codes

---
+++ src/gci/Sets.java
@@ -0,0 +1,23 @@
+package gci;
+
+import java.util.*;
+
+public class Sets {
+    public void mycode(){
+        String [] students={"John","Paul","Adam","Paul"};
+        List<String> mylist = Arrays.asList(students);
+        System.out.printf("%s",mylist);
+        System.out.println();
+        System.out.println("----------");
+        Set<String>set = new HashSet<>(mylist);
+        System.out.printf("%s ",set);
+        System.out.println();
+        for(String ss: set){
+            System.out.printf("%s ",ss);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Sets().mycode();
+    }
+}
